24d5a391d81f6765e676760c4c14d6b35ffc63bd
m.ahmad <mahmad@fireblocks.com>
2025-06-10 16:40:27 +1000

    fix import

--- contracts/bridge-adapter/FireblocksOFTAdapter.sol
+++ contracts/bridge-adapter/FireblocksOFTAdapter.sol
@@ -15,8 +15,8 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 pragma solidity 0.8.22;
 
-import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC-20/extensions/IERC20Metadata.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC-20/utils/SafeERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import {IOFT, OFTCore} from "@layerzerolabs/oft-evm/contracts/OFTCore.sol";
