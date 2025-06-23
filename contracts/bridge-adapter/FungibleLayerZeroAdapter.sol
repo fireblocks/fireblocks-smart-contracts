611fd659ed3a2d1a985658d82252e90d99068b3c
m.ahmad <mahmad@fireblocks.com>
2025-06-23 12:36:06 +1000

    fix: optimize state read

--- contracts/bridge-adapter/FungibleLayerZeroAdapter.sol
+++ contracts/bridge-adapter/FungibleLayerZeroAdapter.sol
@@ -463,10 +463,11 @@ contract FungibleLayerZeroAdapter is OFTCore, RoleBasedOwnable, PauseCapable, Sa
 			_peerEids.push(_eid);
 		} else if (hadPeerBefore && isSettingToZero) {
 			// Removing peer - remove from array
-			for (uint256 i; i < _peerEids.length; ) {
+			uint256 peerCount = _peerEids.length;
+			for (uint256 i; i < peerCount; ) {
 				if (_peerEids[i] == _eid) {
 					// Replace with the last element and pop
-					_peerEids[i] = _peerEids[_peerEids.length - 1];
+					_peerEids[i] = _peerEids[peerCount - 1];
 					_peerEids.pop();
 					break;
 				}
